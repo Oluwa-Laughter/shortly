bc2232f1be7043847425eb8caea1a296e57af187
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-11-05 11:56:11 +0100

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      maxWidth: {
+        container: "1200px",
+      },
+      screens: {
+        xs: "320px",
+        sm: "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "768px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+      },
+    },
+  },
+  plugins: [],
+};
