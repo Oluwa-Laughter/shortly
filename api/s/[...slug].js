255ff44ce867943981e9617962b8d7b2f16495a2
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-11-20 09:58:56 +0100

    update redirect fixes

--- api/s/[...slug].js
+++ api/s/[...slug].js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY;
+
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default async function handler(req, res) {
