bfadd031a97a00e736f3cdb4fd84445c0b574bdb
Oluwa-Laughter <210806073@live.unilag.edu.ng>
2024-11-20 07:41:44 +0100

    fixing shortenedurl

--- api/[slug].js
+++ api/[slug].js
@@ -6,6 +6,7 @@ const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export default async function handler(req, res) {
   const { slug } = req.query;
+  console.log(`Slug: ${slug}`);
 
   const { data, error } = await supabase
     .from("shortly")
@@ -14,8 +15,10 @@ export default async function handler(req, res) {
     .single();
 
   if (error || !data) {
+    console.log(`Error: ${error}`);
     return res.status(404).send("URL not found");
   }
 
+  console.log(`Old URL: ${data.old_url}`);
   res.redirect(data.old_url);
 }
